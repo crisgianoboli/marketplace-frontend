995b580a0f291853d657075360b007dab03906d1
Cris <crisgianoboli@gmail.com>
2024-12-12 20:47:44 -0300

    add login files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,20 @@
 'use client';
 
 import { Provider } from 'react-redux';
+import Link from 'next/link';
 import { store } from './store/store';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>
-        <Provider store={store}>{children}</Provider>
-      </body>
-    </html>
+    <Provider store={store}>
+      <html lang="en">
+        <body>
+          <nav>
+            <Link href="/login">Login</Link> | <Link href="/dashboard">Dashboard</Link>
+          </nav>
+          {children}
+        </body>
+      </html>
+    </Provider>
   );
 }
